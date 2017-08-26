5dae802267131293d1680904616b1d6ec83336a4
Jean-Christophe Fillion-Robin <jchris.fillionr@kitware.com>
2017-08-26 00:21:23 -0400

    COMP: Update loadable module interface to support Qt5
    In Qt5, the Q_EXPORT_PLUGIN2 macro has been deprecated in favor of the
    new Q_PLUGIN_METADATA macro.

--- LightWeightRobotIGT/qSlicerLightWeightRobotIGTModule.cxx
+++ LightWeightRobotIGT/qSlicerLightWeightRobotIGTModule.cxx
@@ -26,7 +26,10 @@
 #include "qSlicerLightWeightRobotIGTModuleWidget.h"
 
 //-----------------------------------------------------------------------------
+#if (QT_VERSION < QT_VERSION_CHECK(5, 0, 0))
+#include <QtPlugin>
 Q_EXPORT_PLUGIN2(qSlicerLightWeightRobotIGTModule, qSlicerLightWeightRobotIGTModule);
+#endif
 
 //-----------------------------------------------------------------------------
 /// \ingroup Slicer_QtModules_ExtensionTemplate
